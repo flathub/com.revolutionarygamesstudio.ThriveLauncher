d000f9685311ed27ae2a4dae8746cd53968f347c
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-11-15 10:06:11 +0200

    Switched update sources script to clone the launcher separately
    instead of including it as a submodule as that seems to break Flathub building

--- update_sources.sh
+++ update_sources.sh
@@ -4,8 +4,24 @@
 # required sdk and dotnet extension versions
 (
     set -e
+
+    # This now clones the launcher as having a submodule with LFS breaks Flathub build bot
+    # https://github.com/flatpak/flatpak-builder/issues/463
+    if [ ! -d Thrive-Launcher ]; then
+        echo "Cloning launcher to get required project files..."
+        git clone https://github.com/Revolutionary-Games/Thrive-Launcher.git Thrive-Launcher
+    fi
+
+    echo "Entering launcher folder"
     cd Thrive-Launcher
+
+    echo "Updating launcher version"
+    git checkout master
+    git pull
+    git submodule update --init --recursive
+
     # Make sure all submodules are cloned and build works
+    echo "Building and extracting nuget packages"
     dotnet build ThriveLauncher.sln
     # We use the whole solution here to make sure the scripts can also run when building
     python3 ../flatpak-builder-tools/dotnet/flatpak-dotnet-generator.py ../nuget_sources.json ThriveLauncher.sln --runtime linux-x64
@@ -15,7 +31,7 @@
     # doesn't then bad backages probably got written. To resolve the
     # problem the bin and obj folders need to be deleted to make a
     # fully fresh build.
-    echo "Running Thrive Launcher to see if packages are correct."
+    echo "Running Thrive Launcher to see if packages are correct"
     echo "Please close the window once it appears"
     dotnet run --project ThriveLauncher
 )
