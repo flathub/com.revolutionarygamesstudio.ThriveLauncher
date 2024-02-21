3112e58b7bf51ad70ef15c40c3930fd677964fb5
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-02-21 15:31:28 +0200

    Update to launcher 2.1.1

--- update_sources.sh
+++ update_sources.sh
@@ -8,7 +8,7 @@ DOTNET_VERSION="8.0.201"
 IMAGE_TYPE="bookworm-slim-amd64"
 IMAGE="mcr.microsoft.com/dotnet/sdk:$DOTNET_VERSION-$IMAGE_TYPE"
 
-LAUNCHER_VERSION="v2.1.0"
+LAUNCHER_VERSION="v2.1.1"
 # LAUNCHER_VERSION="master"
 
 # Run in subshell to prevent this accidentally closing the higher level container (and
